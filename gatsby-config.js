c37322338f53dc7c36fde0f9f6621761db6f55c5
EC2 Default User <ec2-user@ip-172-31-92-216.ec2.internal>
2020-01-12 05:38:05 +0000

    s3 agregado

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,11 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+       
+    resolve: `gatsby-plugin-s3`,
+    options: {
+      bucketName: "my-website-bucket",
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
